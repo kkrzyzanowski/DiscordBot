f2d9725f7eef93e41188aed168e5c34c2e717d02
Karol Krzyżanowski <karol131503@wp.pl>
2026-04-01 18:52:11 +0200

    Initialization discord bot

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord, os
+
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True  # <-- TO JEST KLUCZOWE
+intents.messages = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("!hello"):
+        await message.channel.send("Hello!")
+
+client.run(TOKEN)
